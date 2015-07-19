deabdefb5a848a26e0dd519c6b23396cb737b27c
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-07-19 17:09:59 +0430

    Examples and Practices of Chapter 6

---
+++ examples/chapter6/Practice - 4.rb
@@ -0,0 +1,9 @@
+#For other example, see the file "inner function.rb"
+
+def system
+ def out
+  def print(str)
+   print str
+  end
+ end
+end
---
+++ examples/chapter6/Practice -3.rb
@@ -0,0 +1,10 @@
+def fibonacci(n)
+ if n == 0
+  return 1
+ elsif n == 1
+  return 1
+ else
+  return fibonacci(n - 1) + fibonacci(n - 2)
+ end
+end
+
